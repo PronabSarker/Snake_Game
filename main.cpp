a3d08db5e0aa67d284bf8fedff82a74675361d8c
PronabSarker <44532770+PronabSarker@users.noreply.github.com>
2018-10-28 12:27:31 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,24 @@
 
 int main()
 {
-    int i,x[100],y[100],rx,ry,l,d=2,s=16, k=0;
-    int page=0,a,b;
+    int i,x[100],y[100],rx,ry,l,d=2;
+    int page=0;
     char ara[100], ins[500];
     srand(time(NULL));
+    ///creating graphics window
     DWORD scwidth = GetSystemMetrics(SM_CXSCREEN);
     DWORD scheight = GetSystemMetrics(SM_CYSCREEN);
     initwindow(scwidth, scheight, "The Snake Game");
+
     x[0]= scwidth/2;
     y[0]= scheight/2;
+
     l=5;///initial snake length
+
+    ///updating the snake
     for(i=1; i<l; i++)
     {
-        x[i]= x[i-1]-8;
+        x[i]= x[i-1]-16;
         y[i]= y[0];
     }
      ///food coordinates
@@ -52,22 +57,26 @@ int main()
 
     while(1)
     {
+        ///double buffering
         setactivepage(page);
         setvisualpage(1-page);
+
         cleardevice();
+
         ///creating border
-        setfillstyle(1,1);
-        bar(0,30,scwidth,47);
-        bar(0,47,17,scheight);
-        bar(17,scheight-45,scwidth, scheight-28);
-        bar(scwidth-20,47, scwidth, scheight);
+        setfillstyle(SOLID_FILL,1);
+        bar(0,30,scwidth,47);///UP
+        bar(0,47,17,scheight);///left
+        bar(17,scheight-45,scwidth, scheight-28);///DOWNN
+        bar(scwidth-20,47, scwidth, scheight);///right
 
         ///creating food
-        setfillstyle(1,2);
+        setfillstyle(SOLID_FILL,2);
         bar(rx-8,ry-8, rx+8, ry+8);
 
 
-        ///updating the snake
+
+        ///moving the snake
         for(i=l; i>0; i--)
         {
             x[i]= x[i-1];
@@ -75,15 +84,17 @@ int main()
         }
         ///updating the head
         if(d==0)
-            x[0]= x[0]-s;///left
+            x[0]= x[0]-16;///left`
         else if(d==1)
-            y[0]= y[0]-s; ///up
+            y[0]= y[0]-16; ///up
         else if(d==2)
-            x[0]= x[0]+s;///right
+            x[0]= x[0]+16;///right
         else if(d==3)
-            y[0]= y[0]+s; ///down
+            y[0]= y[0]+16; ///down
+
         ///terminating condition
         if(getpixel(x[0], y[0])==1) break;
+
         ///updating direction
         if(GetAsyncKeyState(VK_RIGHT)&& d!=0)
             d=2;
@@ -94,23 +105,24 @@ int main()
         else if(GetAsyncKeyState(VK_DOWN) && d!=1)
         d=3;
 
+        ///food taken??
         if(getpixel(x[0], y[0])==2)
         {
             rx=rand()%scwidth;
             ry=rand()%scheight;
-           if(rx<=(scwidth/2)) rx+=50;
-           else rx-=50;
+            if(rx<=(scwidth/2)) rx+=50;
+            else rx-=50;
             if(ry<=(scheight/2)) ry+=50;
             else ry-=50;
-            setfillstyle(1,2);
+            setfillstyle(SOLID_FILL,2);
             bar(rx-8,ry-8, rx+8, ry+8);
-            l=l+1;
+            l=l+1;///increase snake length
         }
 
         delay(100-((l-5)*3));///snake speed
-        page=1-page;
+        page=1-page;///double buffering
         ///displaying the snake
-        setfillstyle(1,1);
+        setfillstyle(SOLID_FILL,1);
         for(i=0;i<l; i++)
             bar(x[i]-8, y[i]-8, x[i]+8, y[i]+8);
         ///printing score on graphics window
@@ -124,3 +136,4 @@ int main()
     closegraph();
     return 0;
 }
+
